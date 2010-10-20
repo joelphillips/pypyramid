5871c045225eb05552ed365d148c57523191df98
Joel Phillips <joel.phillips@reading.ac.uk>
2010-10-20 08:00:30 +0100

    ...

--- src/pypyr/physics/stokes.py
+++ src/pypyr/physics/stokes.py
@@ -75,6 +75,65 @@ def stokes2(k, meshevents, v, points):
 #    print np.hstack((points, u))
 #    print u
     return u, uu
+
+
+def stokespressure(k, meshevents, pressures, closedtag, points):
+    vortelts1 = pe.HcurlElements(k)
+    vortelts2 = pe.HcurlElements(k)
+    velelts1 = pe.HdivElements(k)
+    velelts2 = pe.HdivElements(k)
+    pressureelts1 = pe.L2Elements(k)
+    
+    quadrule = pu.pyramidquadrature(k+1)
+    
+    Asys = pa.SymmetricSystem(vortelts1, quadrule, meshevents, [])
+#    Bsys = pa.AsymmetricSystem(velelts1, vortelts2, quadrule, meshevents, [bdytag], [])
+    BsysT = pa.AsymmetricSystem(vortelts2, velelts1, quadrule, meshevents, [], [closedtag])
+    Csys = pa.AsymmetricSystem(pressureelts1,velelts2, quadrule, meshevents, [pressures.keys()], [closedtag])
+    
+    A = Asys.systemMatrix(False)
+    BT = BsysT.systemMatrix(True, False)
+    C = Csys.systemMatrix(False, True)
+    
+    v0 = lambda x,n: np.zeros_like(x)
+    vt = lambda x,n: np.zeros_like(x)
+    
+    BTI, BTE, BTGs = BsysT.processBoundary(BT, {closedtag:v0})
+    CI, CE, CGs = Csys.processBoundary(C, {closedtag:v0})
+    
+    P = Csys.loadVector(lambda x: np.ones((len(x),1,1)))
+#    print "P ",P
+    alltags = pressures.keys() + [closedtag]
+    
+    Gt = Asys.boundaryLoad(dict([(tag,vt) for tag in alltags]), pu.squarequadrature(k+1), pu.trianglequadrature(k+1), False)
+
+#    print "Gt ",Gt
+    print A.shape, BT.shape, C.shape, BTI.shape, BTE[openbdytag].shape, BTGs[openbdytag].shape, CI.shape
+
+    AL = Gt[openbdytag] + BTE[openbdytag] * BTGs[openbdytag]
+ #   print "AL ",AL
+    CL = -CE[openbdytag] * CGs[openbdytag]
+    
+    nvort = A.get_shape()[0]
+    nvel = BTI.get_shape()[1]
+    
+#    S = ss.bmat([[A, -BTI, None],[-BTI.transpose(), None, CI.transpose()],[None, CI, None]])
+#    L = np.vstack((AL,np.zeros((nvel,1)), CL))
+    S = ss.bmat([[A, -BTI, None, None],[-BTI.transpose(), None, CI.transpose(), None],[None, CI, None, P], [None,None,P.transpose(), None]])
+    L = np.vstack((AL,np.zeros((nvel,1)), CL, np.zeros((1,1))))
+    X = ssl.spsolve(S, L)
+    U = X[nvort:(nvort + nvel)]
+#    print "X",X
+#    print "U", U
+#    print "BTGs", BTGs
+#    
+    u = BsysT.evaluate(points, U, BTGs, False)
+#    uu = Asys.evaluate(points, np.eye(nvort)[-2], {}, False)
+#    uu = BsysT.evaluate(points, U, {}, False)
+    uu = BsysT.evaluate(points, np.zeros_like(U), BTGs, False)
+#    print np.hstack((points, u))
+#    print u
+    return u, uu
     
 def stokescubemesh(n, mesh):
     """ Produces the events to construct a mesh consisting of n x n x n cubes, each divided into 6 pyramids"""
